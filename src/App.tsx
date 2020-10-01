db8eff7f98a12ea962dc7958d4ebf608d8758669
Almerindo Paixão <almerindopaixao@gmail.com>
2020-09-30 22:04:22 -0300

    Update header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import './App.css';
 
+import Header from './components/Header';
+
 function App(): JSX.Element {
-  return <h1>Hello Semana DevSuperior</h1>;
+  return (
+    <div>
+      <Header />
+    </div>
+  );
 }
 
 export default App;
